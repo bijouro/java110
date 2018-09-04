b07a1ed1e2bbeb2a709acf682df955bd067af4de
Byeon Jongho <bijouro@naver.com>
2018-09-04 12:45:31 +0900

    반복입력기능 추가

--- java110-project/src/main/java/App.java
+++ java110-project/src/main/java/App.java
@@ -6,19 +6,32 @@ public class App {
         // 1) 키보드 입력 처리할 객체 준비
 
         Scanner keyIn = new Scanner(System.in);
+
+        
+        
         
         // 2) 사용자로부터 회원 정보 입력 받기
-
-        System.out.print("이름: ");
-        String name = keyIn.nextLine();
+        while(true) {
+            System.out.print("이름: ");
+            String name = keyIn.nextLine();
+            
+            System.out.print("이메일: ");
+            String email = keyIn.nextLine();
+            
+            System.out.print("패스워드: ");
+            String password = keyIn.nextLine();
+            
+            System.out.printf("%s, %s, %s\n", name, email, password);
+            
+            System.out.print("계속 하시겠습니까? (Y/n) ");
+            String answer = keyIn.nextLine();
+            if(answer.toLowerCase().equals("n"))
+                break;
+            
+        }
         
-        System.out.print("이메일: ");
-        String email = keyIn.nextLine();
         
-        System.out.print("패스워드: ");
-        String password = keyIn.nextLine();
         
-        System.out.printf("%s, %s, %s\n", name, email, password);
         
         
     }
